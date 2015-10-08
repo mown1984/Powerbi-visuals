89c3129a73ab5499c4a03105f3c5d75ea5f364ac
v-tkalek <v-tkalek@microsoft.com>
2015-10-08 16:36:41 -0700

    removed header

--- src/Clients/Visuals/visuals/samples/sunburst.ts
+++ src/Clients/Visuals/visuals/samples/sunburst.ts
@@ -1,29 +1,4 @@
-﻿/*
- *  Power BI Visualizations
- *
- *  Copyright (c) Microsoft Corporation
- *  All rights reserved. 
- *  MIT License
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the ""Software""), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *   
- *  The above copyright notice and this permission notice shall be included in 
- *  all copies or substantial portions of the Software.
- *   
- *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *  THE SOFTWARE.
- */
-/// <reference path="../../_references.ts"/>
+﻿/// <reference path="../../_references.ts"/>
 
 module powerbi.visuals.samples {
     import SelectionManager = utility.SelectionManager;
@@ -174,10 +149,9 @@ module powerbi.visuals.samples {
             let height = this.viewport.height;
             let percentageText = this.svg.select(".sunBurstPercentage");
             let percentageFixedText = this.svg.select(".sunBurstPercentageFixed");
-            let dXtextFixed = percentageText.text.length > 0 ? 10 : 0;
             percentageText.style("opacity", 1);
             percentageFixedText.style("opacity", 1);
-            percentageFixedText.attr("y", (height / 2 + dXtextFixed));
+            percentageFixedText.attr("y", (height / 2));
             percentageFixedText.attr("x", ((width / 2) - (percentageFixedText.node().clientWidth / 2)));
         }
 
