9ad5ab31843894a9e12543fa8961ab5e382fa517
Ignat Vilesov <v-viig@microsoft.com>
2015-10-28 14:45:13 +0300

    fixes bug 1633: "Unexpected error when turning off step words"

--- src/Clients/Visuals/visuals/samples/wordCloud.ts
+++ src/Clients/Visuals/visuals/samples/wordCloud.ts
@@ -85,6 +85,7 @@ module powerbi.visuals.samples {
         isBrokenText: boolean;
         isRemoveStopWords: boolean;
         stopWords: string;
+        isDefaultStopWords: boolean;
         stopWordsArray: string[];
     }
 
@@ -114,6 +115,10 @@ module powerbi.visuals.samples {
                 show: <DataViewObjectPropertyIdentifier> {
                     objectName: "stopWords",
                     propertyName: "show"
+                },
+                isDefaultStopWords: <DataViewObjectPropertyIdentifier> {
+                    objectName: "stopWords",
+                    propertyName: "isDefaultStopWords"
                 },
                 words: <DataViewObjectPropertyIdentifier> {
                     objectName: "stopWords",
@@ -151,7 +156,7 @@ module powerbi.visuals.samples {
         };
 
         private static Size: string = "px";
-        private static StopWordsDelemiter: string = ",";
+        private static StopWordsDelemiter: string = " ";
 
         private static Radians: number = Math.PI / 180;
 
@@ -221,6 +226,10 @@ module powerbi.visuals.samples {
                             displayName: data.createDisplayNameGetter("Visual_Show"),
                             type: { bool: true }
                         },
+                        isDefaultStopWords: {
+                            displayName: "Default Stop Words",
+                            type: { bool: true }
+                        },
                         words: {
                             displayName: "Words",
                             type: { text: true }
@@ -251,6 +260,20 @@ module powerbi.visuals.samples {
             }
         };
 
+        private static StopWords: string[] = [
+            "a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an",
+            "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot",
+            "could", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get",
+            "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i",
+            "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may",
+            "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often",
+            "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should",
+            "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these",
+            "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what",
+            "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet",
+            "you", "your"
+        ];
+
         private static DefaultSettings: WordCloudSettings = {
             minFontSize: 20,
             maxFontSize: 100,
@@ -260,20 +283,9 @@ module powerbi.visuals.samples {
             isRotateText: true,
             isBrokenText: true,
             isRemoveStopWords: false,
-            stopWordsArray: [
-                "a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an",
-                "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot",
-                "could", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get",
-                "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i",
-                "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may",
-                "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often",
-                "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should",
-                "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these",
-                "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what",
-                "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet",
-                "you", "your", ".", ",", '"', "", ":", ";", "!", "?"
-            ],
-            stopWords: undefined
+            stopWordsArray: [],
+            stopWords: undefined,
+            isDefaultStopWords: false
         };
 
         private settings: WordCloudSettings;
@@ -386,7 +398,8 @@ module powerbi.visuals.samples {
                 isBrokenText: boolean = true,
                 isRemoveStopWords: boolean = true,
                 stopWords: string,
-                stopWordsArray: string[];
+                stopWordsArray: string[],
+                isDefaultStopWords: boolean = false;
 
             minFonSize = this.getNumberFromObjects(
                 objects,
@@ -464,6 +477,11 @@ module powerbi.visuals.samples {
                 stopWordsArray = WordCloud.DefaultSettings.stopWordsArray;
             }
 
+            isDefaultStopWords = DataViewObjects.getValue<boolean>(
+                objects,
+                WordCloud.Properties.stopWords.isDefaultStopWords,
+                WordCloud.DefaultSettings.isDefaultStopWords);
+
             return {
                 minFontSize: minFonSize,
                 maxFontSize: maxFontSize,
@@ -475,7 +493,8 @@ module powerbi.visuals.samples {
                 isBrokenText: isBrokenText,
                 isRemoveStopWords: isRemoveStopWords,
                 stopWords: stopWords,
-                stopWordsArray: stopWordsArray
+                stopWordsArray: stopWordsArray,
+                isDefaultStopWords: isDefaultStopWords
             };
         }
 
@@ -920,8 +939,14 @@ module powerbi.visuals.samples {
                     let words: string[] = item.text.split(" ");
 
                     if (this.settings.isRemoveStopWords) {
+                        let stopWords: string[] = this.settings.stopWordsArray;
+
+                        if (this.settings.isDefaultStopWords) {
+                            stopWords = stopWords.concat(WordCloud.StopWords);
+                        }
+
                         words = words.filter((value: string) => {
-                            return !this.settings.stopWordsArray.some((removeWord: string) => {
+                            return !stopWords.some((removeWord: string) => {
                                 return value.toLocaleLowerCase() === removeWord.toLocaleLowerCase();
                             });
                         });
@@ -1207,6 +1232,7 @@ module powerbi.visuals.samples {
                         selector: null,
                         properties: {
                             show: this.settings.isRemoveStopWords,
+                            isDefaultStopWords: this.settings.isDefaultStopWords,
                             words: this.settings.stopWords ||
                                 this.settings.stopWordsArray.join(WordCloud.StopWordsDelemiter)
                         }
