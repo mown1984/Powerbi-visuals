4bb86ad6656ce7cb3882bce6506ca75efea0e58e
Ignat Vilesov <v-viig@microsoft.com>
2015-11-16 17:20:48 +0300

    fixed bug 1446 "x-axis does not work for categorical. The user should pick a datetime field, but we should support categorical anyway. Currently we render the index for non-date x-axis fields."

--- src/Clients/Visuals/visuals/samples/streamGraph.ts
+++ src/Clients/Visuals/visuals/samples/streamGraph.ts
@@ -27,9 +27,15 @@
 /// <reference path="../../_references.ts"/>
 
 module powerbi.visuals.samples {
+    import ValueFormatter = powerbi.visuals.valueFormatter;
+    import SelectionManager = utility.SelectionManager;
+    import ClassAndSelector = jsCommon.CssConstants.ClassAndSelector;
+
     export interface StreamData {
         dataPoints: StreamDataPoint[][];
         legendData: LegendData;
+        valueFormatter: IValueFormatter;
+        axisXData: StreamAxisXData;
     }
 
     export interface StreamDataPoint {
@@ -39,7 +45,23 @@ module powerbi.visuals.samples {
         identity: SelectionId;
     }
 
+    export interface StreamAxisXData {
+        valueFormatter: IValueFormatter;
+        values: any[];
+        maxNumverOfValues: number;
+        scale?: D3.Scale.GenericScale<D3.Scale.LinearScale | D3.Scale.TimeScale | D3.Scale.OrdinalScale>;
+    }
+
     export class StreamGraph implements IVisual {
+        public static Properties: any = {
+            general: {
+                formatString: <DataViewObjectPropertyIdentifier>{
+                    objectName: 'general',
+                    propertyName: 'formatString'
+                }
+            }
+        };
+
         public static capabilities: VisualCapabilities = {
             dataRoles: [
                 {
@@ -79,36 +101,52 @@ module powerbi.visuals.samples {
                 general: {
                     displayName: 'General',
                     properties: {
+                        formatString: { type: { formatting: { formatString: true } } },
                         wiggle: {
                             type: { bool: true },
                             displayName: 'Wiggle'
                         }
                     }
                 }
-            },
-            //drilldown:{roles:['Series']}
+            }
         };
 
         private static VisualClassName = 'streamGraph';
-        private static Layer: jsCommon.CssConstants.ClassAndSelector = jsCommon.CssConstants.createClassAndSelector('layer');
+
+        private static Layer: ClassAndSelector = {
+            'class': 'layer',
+            selector: '.layer'
+        };
+
+        private static MaxNumberOfAxisXValues: number = 5;
+
+        private margin: IMargin = { left: 45, right: 25, bottom: 25, top: 25 };
+
+        private viewport: IViewport;
 
         private svg: D3.Selection;
-        private axis: D3.Selection;
+        private xAxis: D3.Selection;
+        private yAxis: D3.Selection;
         private colors: IDataColorPalette;
         private selectionManager: utility.SelectionManager;
         private dataView: DataView;
         private legend: ILegend;
 
-        public static converter(dataView: DataView, colors: IDataColorPalette): StreamData {
-            var catDv: DataViewCategorical = dataView.categorical;
-            var values = catDv.values;
-            var dataPoints: StreamDataPoint[][] = [];
-            var legendData: LegendData = {
-                dataPoints: [],
-                title: values[0].source.displayName
-            };
-            for (var i = 0, iLen = values.length; i < iLen; i++) {
+        public converter(dataView: DataView, colors: IDataColorPalette): StreamData {
+            let catDv: DataViewCategorical = dataView.categorical,
+                values: DataViewValueColumns = catDv.values,
+                dataPoints: StreamDataPoint[][] = [],
+                legendData: LegendData = {
+                    dataPoints: [],
+                    title: values[0].source.displayName
+                },
+                categoriesValues: any[] = [],
+                value: number = 0,
+                valueFormatter: IValueFormatter;
+
+            for (let i = 0, iLen = values.length; i < iLen; i++) {
                 dataPoints.push([]);
+
                 legendData.dataPoints.push({
                     label: values[i].source.groupName,
                     color: colors.getColorByIndex(i).value,
@@ -116,129 +154,251 @@ module powerbi.visuals.samples {
                     selected: false,
                     identity: null
                 });
-                for (var k = 0, kLen = values[i].values.length; k < kLen; k++) {
-                    var id = SelectionIdBuilder
-                        .builder()
-                        .withSeries(dataView.categorical.values, dataView.categorical.values[i])
-                        .createSelectionId();
+
+                for (let k = 0, kLen = values[i].values.length; k < kLen; k++) {
+                    let id: SelectionId = SelectionIdBuilder
+                            .builder()
+                            .withSeries(dataView.categorical.values, dataView.categorical.values[i])
+                            .createSelectionId(),
+                        y: number = values[i].values[k];
+
+                    if (y > value) {
+                        value = y;
+                    }
+
                     dataPoints[i].push({
                         x: k,
-                        y: values[i].values[k],
+                        y: y,
                         identity: id
                     });
                 }
             }
 
+            valueFormatter = ValueFormatter.create({
+                format: "g",
+                value: value
+            });
+
+            if (catDv.categories &&
+                catDv.categories[0] &&
+                catDv.categories[0].values) {
+                categoriesValues = catDv.categories[0].values;
+            }
+
             return {
                 dataPoints: dataPoints,
-                legendData: legendData
+                legendData: legendData,
+                valueFormatter: valueFormatter,
+                axisXData: this.parseAxisXData(
+                    catDv.categories[0].source,
+                    categoriesValues)
+            };
+        }
+
+        private parseAxisXData(source: DataViewMetadataColumn, values: any[]): StreamAxisXData {
+            let maxNumverOfValues: number = StreamGraph.MaxNumberOfAxisXValues;
+
+            if (!values || !(values.length > 0)) {
+                return {
+                    valueFormatter: null,
+                    values: [],
+                    maxNumverOfValues: maxNumverOfValues
+                };
+            }
+
+            let scale: D3.Scale.LinearScale | D3.Scale.TimeScale | D3.Scale.OrdinalScale,
+                valueFormatter: IValueFormatter = ValueFormatter.create({
+                    format: ValueFormatter.getFormatString(
+                        source,
+                        StreamGraph.Properties.general.formatString),
+                    value: values[0]
+                }),
+                range: any[] = [this.margin.left, this.viewport.width - this.margin.right],
+                filteredValues: any[] = values;
+
+            if (source.type.dateTime) {
+                scale = d3.time.scale();
+
+                scale.domain([values[0], values[values.length - 1]]);
+                scale.range(range);
+            } else if (source.type.numeric) {
+                scale = d3.scale.linear();
+
+                scale.domain([values[0], values[values.length - 1]]);
+                scale.range(range);
+            } else {
+                let shiftIndex: number = 0;
+
+                filteredValues = [];
+
+                scale = d3.scale.ordinal();
+
+                if (values.length % 2 > 0) {
+                    shiftIndex = Math.floor(values.length / 2);
+                } else {
+                    shiftIndex = values.length - 1;
+                }
+
+                shiftIndex = Math.max(1, shiftIndex);
+
+                for (let index: number = 0; index < values.length; index += shiftIndex) {
+                    filteredValues.push(values[index]);
+                }
+
+                scale.domain(filteredValues);
+                (<D3.Scale.OrdinalScale> scale).rangePoints(range);
+            }
+
+            return {
+                valueFormatter: valueFormatter,
+                values: filteredValues,
+                maxNumverOfValues: maxNumverOfValues,
+                scale: scale
             };
         }
 
         public init(options: VisualInitOptions): void {
-            var element = options.element;
-            this.selectionManager = new utility.SelectionManager({ hostServices: options.host });
+            let element: JQuery = options.element;
+
+            this.selectionManager = new SelectionManager({ hostServices: options.host });
+
             this.svg = d3.select(element.get(0))
                 .append('svg')
                 .classed(StreamGraph.VisualClassName, true);
 
-            this.axis = this.svg.append("g");
+            this.xAxis = this.svg.append("g");
+            this.yAxis = this.svg.append("g");
 
             this.colors = options.style.colorPalette.dataColors;
 
-            this.legend = createLegend(element,false, null);
+            this.legend = createLegend(element, false, null);
         }
 
-        public update(options: VisualUpdateOptions) {
-            if (!options.dataViews || !options.dataViews[0]) return;
-            var duration = options.suppressAnimations ? 0 : 250;
+        public update(options: VisualUpdateOptions): void {
+            if (!options.dataViews || !options.dataViews[0]) {
+                return;
+            };
 
-            var dataView = this.dataView = options.dataViews[0];
-            var data = StreamGraph.converter(dataView, this.colors);
-            var dataPoints = data.dataPoints;
-            var viewport = options.viewport;
-            var margins: IMargin = { left: 20, right: 20, bottom: 25, top: 25 };
+            this.viewport = options.viewport;
 
-            this.legend.drawLegend(data.legendData, viewport);
+            let duration: number = options.suppressAnimations ? 0 : 250,
+                dataView: DataView = this.dataView = options.dataViews[0],
+                data: StreamData = this.converter(dataView, this.colors),
+                dataPoints: StreamDataPoint[][] = data.dataPoints;
 
-            var height = options.viewport.height - margins.top;
+            if (dataPoints.length === 0 ) {
+                this.svg.selectAll(StreamGraph.Layer.selector).remove();
+
+                return;
+            }
+
+            this.legend.drawLegend(data.legendData, this.viewport);
+
+            let height: number = options.viewport.height - this.margin.top;
 
             this.svg.attr({
-                'width': viewport.width,
+                'width': this.viewport.width,
                 'height': height
             });
 
-            var stack = d3.layout.stack();
+            let stack: D3.Layout.StackLayout = d3.layout.stack();
 
-            if (this.getWiggle(dataView)) 
+            if (this.getWiggle(dataView)) {
                 stack.offset('wiggle'); 
-            var layers = stack(dataPoints);
+            }
 
-            var x = d3.scale.linear()
+            let layers: StreamDataPoint[][] = stack(dataPoints);
+
+            let xScale: D3.Scale.LinearScale = d3.scale.linear()
                 .domain([0, dataPoints[0].length - 1])
-                .range([margins.left, viewport.width - margins.right]);
+                .range([this.margin.left, this.viewport.width - this.margin.right]);
 
-            var y = d3.scale.linear()
-                .domain([0, d3.max(layers,(layer) => {
-                return d3.max(layer,(d) => {
-                    return d.y0 + d.y;
-                });
-            })]).range([height - margins.bottom, margins.top]);
+            let yScale: D3.Scale.LinearScale = d3.scale.linear()
+                .domain([0, d3.max(layers, (layer) => {
+                    return d3.max(layer, (d) => {
+                        return d.y0 + d.y;
+                    });
+                })])
+                .range([height - this.margin.bottom, this.margin.top]);
 
-            var area = d3.svg.area()
+            let area: D3.Svg.Area = d3.svg.area()
                 .interpolate('basis')
-                .x(d => x(d.x))
-                .y0(d => y(d.y0))
-                .y1(d => y(d.y0 + d.y));
+                .x(d => xScale(d.x))
+                .y0(d => yScale(d.y0))
+                .y1(d => yScale(d.y0 + d.y));
 
-            var sm = this.selectionManager;
-            var selection = this.svg.selectAll(StreamGraph.Layer.selector)
+            let selectionManager: SelectionManager = this.selectionManager;
+
+            let selection: D3.UpdateSelection = this.svg.selectAll(StreamGraph.Layer.selector)
                 .data(layers);
 
             selection.enter()
                 .append('path')
-                .classed(StreamGraph.Layer.class, true);
+                .classed(StreamGraph.Layer["class"], true);
 
             selection
                 .style("fill",(d, i) => this.colors.getColorByIndex(i).value)
                 .on('click', function (d) {
-                sm.select(d[0].identity).then(ids=> {
-                    if (ids.length > 0) {
-                        selection.style('opacity', 0.5);
-                        d3.select(this).style('opacity', 1);
-                    } else {
-                        selection.style('opacity', 1);
-                    }
-                });
-            })
+                    selectionManager.select(d[0].identity).then(ids=> {
+                        if (ids.length > 0) {
+                            selection.style('opacity', 0.5);
+                            d3.select(this).style('opacity', 1);
+                        } else {
+                            selection.style('opacity', 1);
+                        }
+                    });
+                })
                 .transition()
                 .duration(duration)
                 .attr("d", area);
 
             selection.exit().remove();
 
-            this.drawAxis(viewport, margins);
+            this.drawAxis(data, yScale);
         }
 
-        private drawAxis(viewport: IViewport, margins: IMargin) {
-            var dataView = this.dataView;
-            var xS = d3.time.scale();
-            var values = dataView.categorical.categories[0].values;
-            xS.domain([values[0], values[values.length - 1]])
-                .range([margins.left, viewport.width - margins.right]);
+        private drawAxis(data: StreamData, yScale: D3.Scale.LinearScale) {
+            let shiftY: number = this.viewport.height - this.margin.bottom - this.margin.top;
 
-            var xAxis = d3.svg.axis().scale(xS).ticks(5);
+            let xAxis: D3.Svg.Axis = d3.svg.axis()
+                .scale(data.axisXData.scale)
+                .orient("bottom")
+                .ticks(StreamGraph.MaxNumberOfAxisXValues)
+                .tickFormat((item: any) => {
+                    if (data.axisXData.valueFormatter) {
+                        return data.axisXData.valueFormatter.format(item);
+                    }
 
-            this.axis.attr("class", "x axis")
-                .attr("transform", "translate(0," + (viewport.height - margins.bottom - margins.top) + ")")
+                    return item;
+                });
+
+            let yAxis: D3.Svg.Axis = d3.svg.axis()
+                .scale(yScale)
+                .orient("left")
+                .tickFormat((item: any) => {
+                    if (data.valueFormatter) {
+                        return data.valueFormatter.format(item);
+                    }
+
+                    return item;
+                });
+
+            this.xAxis.attr("class", "x axis")
+                .attr("transform", SVGUtil.translate(0, shiftY))
                 .call(xAxis);
+
+            this.yAxis.attr("class", "y axis")
+                .attr("transform", SVGUtil.translate(this.margin.left, 0))
+                .call(yAxis);
         }
 
         private getWiggle(dataView: DataView) {
             if (dataView) {
-                var objects = dataView.metadata.objects;
+                let objects = dataView.metadata.objects;
+
                 if (objects) {
-                    var general = objects['general'];
+                    let general = objects['general'];
+
                     if (general) {
                         return <boolean>general['wiggle'];
                     }
@@ -249,11 +409,12 @@ module powerbi.visuals.samples {
         }
 
         public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] {
-            var instances: VisualObjectInstance[] = [];
-            var dataView = this.dataView;
+            let instances: VisualObjectInstance[] = [],
+                dataView = this.dataView;
+
             switch (options.objectName) {
                 case 'general':
-                    var general: VisualObjectInstance = {
+                    let general: VisualObjectInstance = {
                         objectName: 'general',
                         displayName: 'General',
                         selector: null,
@@ -261,6 +422,7 @@ module powerbi.visuals.samples {
                             wiggle: this.getWiggle(dataView)
                         }
                     };
+
                     instances.push(general);
                     break;
             }
