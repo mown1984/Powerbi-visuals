866d079b1369aff608563e86a2e5746b3f13fbf6
Denis Kudelin <v-dekude@microsoft.com>
2016-01-18 14:54:03 +0300

    Bullet Chart Test: 2837 Add sanity test for Bullet Chart

---
+++ src/Clients/PowerBIVisualsTests/customVisuals/sampleDataViews/BulletChartData.ts
@@ -0,0 +1,135 @@
+﻿/*
+ *  Power BI Visualizations
+ *
+ *  Copyright (c) Microsoft Corporation
+ *  All rights reserved. 
+ *  MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the ""Software""), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *   
+ *  The above copyright notice and this permission notice shall be included in 
+ *  all copies or substantial portions of the Software.
+ *   
+ *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+/// <reference path="../../_references.ts"/>
+
+module powerbitests.customVisuals.sampleDataViews {
+    import SQExprBuilder = powerbi.data.SQExprBuilder;
+
+    export function bulletChartData(): powerbi.DataView {
+        var dataViewMetadata: powerbi.DataViewMetadata = {
+            columns: [
+                {
+                    displayName: 'Category',
+                    roles: { 'Category': true },
+                    type: powerbi.ValueType.fromDescriptor({ text: true }),
+                }, {
+                    displayName: 'Value',
+                    roles: { 'Value': true },
+                    isMeasure: true,
+                    type: powerbi.ValueType.fromDescriptor({ numeric: true }),
+                }, {
+                    displayName: 'Target Value',
+                    roles: { 'TargetValue': true },
+                    isMeasure: true,
+                    type: powerbi.ValueType.fromDescriptor({ numeric: true }),
+                }, {
+                    displayName: 'Minimum',
+                    roles: { 'Minimum': true },
+                    isMeasure: true,
+                    type: powerbi.ValueType.fromDescriptor({ numeric: true }),
+                }, {
+                    displayName: 'Satisfactory',
+                    roles: { 'Satisfactory': true },
+                    isMeasure: true,
+                    type: powerbi.ValueType.fromDescriptor({ numeric: true }),
+                }, {
+                    displayName: 'Good',
+                    roles: { 'Good': true },
+                    isMeasure: true,
+                    type: powerbi.ValueType.fromDescriptor({ numeric: true }),
+                }, {
+                    displayName: 'Maximum',
+                    roles: { 'Maximum': true },
+                    isMeasure: true,
+                    type: powerbi.ValueType.fromDescriptor({ numeric: true }),
+                }
+            ]
+        };
+
+        var columns = [
+            {
+                source: dataViewMetadata.columns[1],
+                // Value
+                values: [2, 4, 3, 3, 4, 3, 4, 5],
+            },
+            {
+                source: dataViewMetadata.columns[2],
+                // TargetValue
+                values: [3, 3, 3, 2, 2, 2, 3, 3],
+            },
+            {
+                source: dataViewMetadata.columns[3],
+                // Minimum
+                values: [1,1 ,1 ,1 ,1 ,1 ,2 ,2],
+            },
+            {
+                source: dataViewMetadata.columns[4],
+                // Satisfactory
+                values: [2, 2 ,2 ,3 ,3 ,3 ,3 ,3],
+            },
+            {
+                source: dataViewMetadata.columns[5],
+                // Good
+                values: [4, 4, 4 ,6 ,6 ,6 ,4 ,4],
+            },
+            {
+                source: dataViewMetadata.columns[6],
+                // Maximum
+                values: [6, 6, 6, 8, 8, 8, 8, 7],
+            }
+        ];
+
+        var fieldExpr = SQExprBuilder.fieldExpr({ column: { schema: 's', entity: "Category", name: "Category" } });
+
+        var categoryValues = ["One", "Two", "Three", "Four", "Five", "Six", "Seven"];
+        var categoryIdentities = categoryValues.map((value) => 
+            powerbi.data.createDataViewScopeIdentity(SQExprBuilder.equal(fieldExpr, SQExprBuilder.text(value))));
+
+        var dataValues: powerbi.DataViewValueColumns = powerbi.data.DataViewTransform.createValueColumns(columns);
+        var tableDataValues = categoryValues.map((category, idx) => {
+            var categoryDataValues = columns.map(x => <any>x.values[idx]);
+            categoryDataValues.unshift(category);
+            return categoryDataValues;
+        });
+
+        return {
+            metadata: dataViewMetadata,
+            categorical: {
+                categories: [{
+                    source: dataViewMetadata.columns[0],
+                    values: categoryValues,
+                    identity: categoryIdentities,
+                }],
+                values: dataValues
+            },
+            table: {
+                rows: tableDataValues,
+                columns: dataViewMetadata.columns,
+            }
+        };
+    }
+}
