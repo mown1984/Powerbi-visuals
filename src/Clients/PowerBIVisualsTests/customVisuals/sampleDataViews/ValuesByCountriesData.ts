4686abd990bd06ad4c1b14e686444c8d2c267f34
Denis Kudelin <v-dekude@microsoft.com>
2016-01-25 18:37:58 +0300

    Sankey Diagram Test: 2847 Add sanity test for Sankey Diagram

---
+++ src/Clients/PowerBIVisualsTests/customVisuals/sampleDataViews/ValuesByCountriesData.ts
@@ -0,0 +1,95 @@
+﻿/*
+ *  Power BI Visualizations
+ *
+ *  Copyright (c) Microsoft Corporation
+ *  All rights reserved. 
+ *  MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the ""Software""), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *   
+ *  The above copyright notice and this permission notice shall be included in 
+ *  all copies or substantial portions of the Software.
+ *   
+ *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+/// <reference path="../../_references.ts"/>
+
+module powerbitests.customVisuals.sampleDataViews {
+    import DataViewMetadata = powerbi.DataViewMetadata;
+    import DataView = powerbi.DataView;
+    import ValueType = powerbi.ValueType;
+    import DataViewTransform = powerbi.data.DataViewTransform;
+
+    export class ValuesByCountriesData {
+
+        public getDataView(): DataView {
+            let sourceData: string[] = [
+                "Brazil", "Brazil", "Brazil", "Brazil", "Canada", "Canada",
+                "Canada", "Mexico", "Mexico", "Mexico", "Mexico", "USA",
+                "USA", "USA", "USA", "Portugal", "Portugal", "Portugal"
+            ];
+
+            let destinationData: string[] = [
+                "Portugal", "France", "Spain", "England", "Portugal", "France",
+                "England", "Portugal", "France", "Spain", "England", "Portugal",
+                "France", "Spain", "England", "Angola", "Senegal", "Morocco"
+            ];
+
+            let sampleData: number[] = [
+                5, 1, 1, 1, 1, 5, 1, 1, 1,
+                5, 1, 1, 1, 1, 5, 2, 1, 1
+            ];
+
+            let dataViewMetadata: DataViewMetadata = {
+                columns: [
+                    {
+                        displayName: "Source",
+                        queryName: "Sankey.Source",
+                        type: ValueType.fromDescriptor({ text: true })
+                    },
+                    {
+                        displayName: "Destination",
+                        queryName: "Sankey.Destination",
+                        type: ValueType.fromDescriptor({ text: true })
+                    },
+                    {
+                        displayName: "Value",
+                        queryName: "Sum(Sankey.Value)"
+                    }]
+            };
+
+            return {
+                metadata: dataViewMetadata,
+                categorical: {
+                    categories: [
+                        {
+                            source: dataViewMetadata.columns[0],
+                            values: sourceData
+                        },
+                        {
+                            source: dataViewMetadata.columns[1],
+                            values: destinationData
+                        }],
+                    values: DataViewTransform.createValueColumns([
+                        {
+                            source: dataViewMetadata.columns[2],
+                            values: sampleData
+                        }
+                    ])
+                }
+            };
+        }
+    }
+}
---
+++ src/Clients/PowerBIVisualsTests/customVisuals/sankeyDiagramTests.ts
@@ -0,0 +1,91 @@
+﻿/*
+ *  Power BI Visualizations
+ *
+ *  Copyright (c) Microsoft Corporation
+ *  All rights reserved. 
+ *  MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the ""Software""), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *   
+ *  The above copyright notice and this permission notice shall be included in 
+ *  all copies or substantial portions of the Software.
+ *   
+ *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+/// <reference path="../_references.ts"/>
+
+module powerbitests.customVisuals {
+    import VisualClass = powerbi.visuals.samples.SankeyDiagram;
+
+    describe("SankeyDiagram", () => {
+        describe('capabilities', () => {
+            it("registered capabilities", () => expect(VisualClass.capabilities).toBeDefined());
+        });
+
+        describe("DOM tests", () => {
+            let visualBuilder: SankeyDiagramBuilder;
+            let dataViews: powerbi.DataView[];
+
+            beforeEach(() => {
+                visualBuilder = new SankeyDiagramBuilder();
+                dataViews = [new powerbitests.customVisuals.sampleDataViews.ValuesByCountriesData().getDataView()];
+            });
+
+            it("main element created", () => expect(visualBuilder.mainElement[0]).toBeInDOM());
+            it("update", (done) => {
+                visualBuilder.update(dataViews);
+                setTimeout(() => {
+
+                    expect(visualBuilder.linksElement).toBeInDOM();
+                    expect(visualBuilder.linksElement.children("path.link").length)
+                        .toBe(dataViews[0].categorical.categories[0].values.length);
+
+                    let allCountries: string[] = dataViews[0].categorical.categories[0].values.concat(dataViews[0].categorical.categories[1].values);
+                    let uniqueCountries = allCountries.sort().filter((value, index, array) => !index || value !== array[index - 1]);
+
+                    expect(visualBuilder.nodesElement).toBeInDOM();
+                    expect(visualBuilder.nodesElement.children("g.node").length)
+                        .toEqual(uniqueCountries.length);
+
+                    done();
+                }, DefaultWaitForRender);
+            });
+        });
+    });
+
+    class SankeyDiagramBuilder extends VisualBuilderBase<VisualClass> {
+        constructor(height: number = 200, width: number = 300, isMinervaVisualPlugin: boolean = false) {
+            super(height, width, isMinervaVisualPlugin);
+            this.build();
+            this.init();
+        }
+
+        public get mainElement() {
+            return this.element.children("svg.sankeyDiagram");
+        }
+
+        public get nodesElement() {
+            return this.mainElement.children("g").children("g.nodes");
+        }
+
+        public get linksElement() {
+            return this.mainElement.children("g").children("g.links");
+        }
+
+        private build(): void {
+            this.visual = new VisualClass();
+        }
+    }
+}
